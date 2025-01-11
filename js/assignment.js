96cac5cba8c8229e246e73f08b10ca9d01c3d7b9
Bri A <alexiellis22@gmail.com>
2025-01-11 17:25:41 -0500

    Create returnTheRemainder function/Update Readme

--- js/assignment.js
+++ js/assignment.js
@@ -1,8 +1,15 @@
 "use strict";
-
+/* 
 const submissionBtn = document.getElementById("submission-btn");
 const remainderElement = document.getElementById("remainder");
 const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+ */
+
+function returnTheRemainder(a,b) {
+    return a % b;
+}
+
+console.log(returnTheRemainder(5,4));
