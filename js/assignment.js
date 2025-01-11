45733c820837ddd25b4c664503a3b0ac1f45db36
Bri A <alexiellis22@gmail.com>
2025-01-11 18:47:10 -0500

    Create functionality to render answers

--- js/assignment.js
+++ js/assignment.js
@@ -1,21 +1,21 @@
 "use strict";
-/* 
+
 const submissionBtn = document.getElementById("submission-btn");
 const remainderElement = document.getElementById("remainder");
 const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
-*/
+
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
 
 
+// returnTheRemainder Function
+
 function returnTheRemainder(a,b) {
-    return a % b;
+        return  a % b;
 }
 
-console.log(returnTheRemainder(5,4));
-
-
+// checkIfEven Function
 
 function checkIfEven(num) {
     if(num % 2 === 0){
@@ -26,10 +26,22 @@ function checkIfEven(num) {
     }  
 }
 
-console.log(checkIfEven(47));
+// getTheFourthElement Function
 
 function getTheFourthElement() {
    return lostNumbers[3];
 }
 
-console.log(getTheFourthElement());
+
+
+
+function render() {
+    remainderElement.innerText = returnTheRemainder(5,4);
+    checkIfEvenElement.innerText = checkIfEven(47);
+    lostNumbersElement.innerText =  getTheFourthElement();
+   
+}
+
+submissionBtn.addEventListener("click", function(){
+    render();
+})
